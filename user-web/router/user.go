7e37146d2a41e635a259754feaa60b0a229b50ec
Shenchangxin <2863727114@qq.com>
2024-07-15 22:21:52 +0800

    完善jwt的集成鉴权，添加校验是否为管理员用户的校验

--- user-web/router/user.go
+++ user-web/router/user.go
@@ -3,10 +3,11 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"go-blog-api/user-web/api"
+	"go-blog-api/user-web/middlewares"
 )
 
 func InitUserRouter(Router *gin.RouterGroup) {
 	UserRouter := Router.Group("user")
-	UserRouter.GET("list", api.GetUserList)
+	UserRouter.GET("list", middlewares.JWTAuth(), middlewares.IsAdminAuth(), api.GetUserList)
 	UserRouter.POST("login", api.Login)
 }
